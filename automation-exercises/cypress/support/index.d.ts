c4cf51e60764b31983b62989374bd38d6e217ef4
Sofia Leal <156724109+sofialeals@users.noreply.github.com>
2025-12-16 14:22:03 -0300

    add template files

---
+++ automation-exercises/cypress/support/index.d.ts
@@ -0,0 +1,6 @@
+// sample interface declaration code
+// declare namespace Cypress {
+//     interface Chainable<Subject = any> {
+//         visitAndLogin(url: string, email: string, password: string): Chainable<any>;
+//     }
+// }
